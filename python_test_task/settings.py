ce98da59a54d1c335c8aaf1a4e8e1c733350fced
Mykola Riabchenko <mriabchenko11@gmail.com>
2021-02-19 07:42:06 +0200

    debug true

--- python_test_task/settings.py
+++ python_test_task/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "os.environ.get('SECRET-KEY')"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["python-magic-link.herokuapp.com", "127.0.0.1"]
 # Application definition
